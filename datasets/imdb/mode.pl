04ca3ad21113d120cd56f7698a41a200c5b85e78
Thais Luca <thais.luca95@gmail.com>
2023-06-17 09:41:52 -0300

    Setting mode file to IMDB dataset

--- datasets/imdb/mode.pl
+++ datasets/imdb/mode.pl
@@ -1,10 +1,17 @@
-workedunder(+person,+person).
-workedunder(+person,-person).
-workedunder(-person,+person).
-female(+person).
-actor(+person).
-director(+person).
-movie(+movie,+person).
-movie(+movie,-person).
-movie(-movie,+person).
-genre(+person,+genre).
+
+:- modeh(1,workedunder(+person,+person)).
+
+:- modeb(*,female(+person)).
+:- modeb(*,actor(+person)).
+:- modeb(*,director(+person)).
+:- modeb(*,genre(+person,+genre)).
+:- modeb(*,movie(+movie,+person)).
+:- modeb(*,movie(+movie,+person)).
+:- modeb(*,movie(+movie,-person)).
+:- modeb(*,movie(-movie,+person)).
+
+:- determination(workedunder/2,female/1).
+:- determination(workedunder/2,actor/1).
+:- determination(workedunder/2,director/1).
+:- determination(workedunder/2,movie/2).
+:- determination(workedunder/2,genre/2).
